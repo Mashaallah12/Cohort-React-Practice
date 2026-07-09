817a3b666a11caf021895293eceac3bd6b002320
Mashaallah <mashaallahmohammad900@gmail.com>
2026-07-10 00:18:03 +0530

    addnew code

---
+++ Form-handling-rendering/src/components/Login.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Login = ({setToggle}) => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-sm bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+
+        <form className="space-y-4">
+          {/* Email */}
+          <div>
+            <label className="block mb-1 text-sm font-medium">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block mb-1 text-sm font-medium">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {/* Remember + Forgot */}
+          <div className="flex justify-between items-center text-sm">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" />
+              Remember me
+            </label>
+
+            <a href="#" className="text-blue-600 hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+
+          {/* Login Button */}
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="text-center text-sm mt-5">
+          Don't have an account?{" "}
+          <a onClick={()=>setToggle((prev) => !prev)} href="#" className="text-blue-600 hover:underline">
+            Register ?
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Form-handling-rendering/src/components/Register.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+const Register = ({ setToggle }) => {
+  let [formData, setFormData] = useState({});
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+   console.log(formData);
+   
+    
+  }
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-sm bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {/* Email */}
+          <div>
+            <label className="block mb-1 text-sm font-medium">Email</label>
+            <input
+                name="email"
+              onChange={handleChange}
+              type="email"
+              placeholder="Enter your email"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="block mb-1 text-sm font-medium">Password</label>
+            <input
+            name="password"
+              onChange={handleChange}
+              type="password"
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {/* Remember + Forgot */}
+          <div className="flex justify-between items-center text-sm">
+            <label className="flex items-center gap-2">
+              <input type="checkbox" />
+              Remember me
+            </label>
+
+            <a href="#" className="text-blue-600 hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+
+          {/* Register Button */}
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+          >
+            Register
+          </button>
+        </form>
+
+        <p className="text-center text-sm mt-5">
+          Don't have an account?{" "}
+          <a
+            onClick={() => setToggle((prev) => !prev)}
+            href="#"
+            className="text-blue-600 hover:underline"
+          >
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
